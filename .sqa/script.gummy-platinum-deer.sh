33c2f57d1e62ea29cb0b29b98f9f20147524ed7d
orviz <orviz@ifca.unican.es>
2022-10-04 10:55:49 +0200

    Add JePL file structure

---
+++ .sqa/script.gummy-platinum-deer.sh
@@ -0,0 +1,4 @@
+(
+cd github.com/dianagudu/motley_cue &&
+    find_doc_files.py --file_type all
+)
---
+++ .sqa/script.pasty-violet-rhinoceros.sh
@@ -0,0 +1,4 @@
+(
+cd github.com/dianagudu/motley_cue &&
+    git rev-parse --is-inside-work-tree
+)
